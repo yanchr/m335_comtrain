d039f54cf9049a398ba364076be82e6fe45aa86c
Christen, Yanick-MGB-M0365530 <yanick.christen@mgb.ch>
2021-02-27 15:43:37 +0100

    commited Project

---
+++ app/src/main/java/ch/zli/m335/comtrain/MainActivity.java
@@ -0,0 +1,14 @@
+package ch.zli.m335.comtrain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
