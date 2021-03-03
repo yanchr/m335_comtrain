7abc4751531e89026f5623eb2aa60e7e8ab23a2d
Christen, Yanick-MGB-M0365530 <yanick.christen@mgb.ch>
2021-03-03 22:14:02 +0100

    -implemented Goal input of exercise and round.

--- app/src/main/java/ch/zli/m335/comtrain/WorkoutActivity.java
+++ app/src/main/java/ch/zli/m335/comtrain/WorkoutActivity.java
@@ -4,13 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Objects;
 
 public class WorkoutActivity extends AppCompatActivity {
 
     Intent startMainActivity;
     Intent startPushUpActivity;
     Intent startStepCounterActivity;
+
+    private final int exerciseGoalInt = 3;
+    private final int roundGoalInt = 6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +41,34 @@ public class WorkoutActivity extends AppCompatActivity {
         MainSvg.setOnClickListener(v -> changeActivities(startMainActivity));
         PushUpSvg.setOnClickListener(v -> changeActivities(startPushUpActivity));
         StepCounterSvg.setOnClickListener(v -> changeActivities(startStepCounterActivity));
+
+        inputGoals();
     }
 
     public void changeActivities(Intent startactivity){
         startActivity(startactivity);
         finish();
+    }
+
+    public void inputGoals(){
+        TextView exerciseGoalView = findViewById(R.id.exerciseGoal);
+        TextView roundGoalView = findViewById(R.id.roundGoal);
+        TextInputEditText exerciseInput = findViewById(R.id.exerciseInput);
+        TextInputEditText roundInput = findViewById(R.id.roundInput);
+        Button startWorkoutBtn = findViewById(R.id.startWorkoutBtn);
+
+        startWorkoutBtn.setOnClickListener(v -> {
+            inputGoal(exerciseGoalView, exerciseInput, exerciseGoalInt);
+            inputGoal(roundGoalView, roundInput, roundGoalInt);
+        });
+    }
+
+    public void inputGoal(TextView goalView, TextInputEditText goalInput, int defaultInt){
+            if (Objects.requireNonNull(goalInput.getText()).length() > 0) {
+                goalView.setText(String.valueOf(goalInput.getText()));
+            } else {
+                goalView.setText(String.valueOf(defaultInt));
+            }
     }
 
 
