7ad2977e1687eb96789bd746f6a10dd75b37e6e3
Christen, Yanick-MGB-M0365530 <yanick.christen@mgb.ch>
2021-03-04 11:01:27 +0100

    -implemented Push up counter

--- app/src/main/java/ch/zli/m335/comtrain/PushUpActivity.java
+++ app/src/main/java/ch/zli/m335/comtrain/PushUpActivity.java
@@ -2,17 +2,34 @@ package ch.zli.m335.comtrain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class PushUpActivity extends AppCompatActivity {
+public class PushUpActivity extends AppCompatActivity implements SensorEventListener {
 
     Intent startMainActivity;
     Intent startStepCounterActivity;
     Intent startWorkoutActivity;
+
+    SensorManager sensorManager;
+    Sensor sensor;
+
+    double downLightNumber;
+    double upLightNumber;
+    int pushUps;
+    boolean wasDown = false;
+    boolean isPaused = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +43,11 @@ public class PushUpActivity extends AppCompatActivity {
         startMainActivity = new Intent(this, MainActivity.class);
         startStepCounterActivity = new Intent(this, StepCoutnerActivity.class);
         startWorkoutActivity = new Intent(this, WorkoutActivity.class);
+
+        activateLightSensor();
+        enableResetBtnListener();
+        enablePauseBtnListener();
+
 
         ImageView MainSvg = findViewById(R.id.MainToMain);
         ImageView StepCounterSvg = findViewById(R.id.MainToStepCounter);
@@ -53,10 +75,92 @@ public class PushUpActivity extends AppCompatActivity {
         });
     }
 
-    public void changeActivities(Intent startactivity){
+    public void changeActivities(Intent startactivity) {
         startActivity(startactivity);
         finish();
     }
 
+    public void activateLightSensor() {
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sensorManager.registerListener((SensorEventListener) this, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        TextView pushUpCounterView = findViewById(R.id.pushupCounterView);
+        Button downCalibratorBtn = findViewById(R.id.downCalibratorBtn);
+        Button upCalibratorBtn = findViewById(R.id.upCalibratorBtn);
+        TextView lightNumberView = findViewById(R.id.lightNumberView);
+        lightNumberView.setText(toString(event.values[0]));
+        downCalibratorBtn.setOnClickListener(v -> {
+            downCalibratorBtn.setText(toString(event.values[0]));
+            downLightNumber = event.values[0];
+        });
+        upCalibratorBtn.setOnClickListener(v -> {
+            upCalibratorBtn.setText(toString(event.values[0]));
+            upLightNumber = event.values[0];
+        });
+
+        if (event.values[0] <= downLightNumber) {
+            wasDown = true;
+        }
+        if (event.values[0] >= upLightNumber && wasDown) {
+            wasDown = false;
+            pushUps++;
+            pushUpCounterView.setText(toString(pushUps));
+        }
+
+    }
+
+    public void enableResetBtnListener() {
+        Button resetBtn = findViewById(R.id.pushUpResetCounterBtn);
+        TextView counterView = findViewById(R.id.pushupCounterView);
+
+        resetBtn.setOnClickListener(v -> {
+            pushUps = 0;
+            counterView.setText(toString(pushUps));
+
+        });
+    }
+
+    public void enablePauseBtnListener() {
+        Button pauseBtn = findViewById(R.id.pauseCounterBtn);
+        pauseBtn.setOnClickListener(v -> {
+            isPaused = !isPaused;
+            if (!isPaused) {
+                onPause();
+                pauseBtn.setText("UNPAUSE");
+            } else{
+                onResume();
+                pauseBtn.setText("PAUSE");
+            }
+        });
+
+    }
+
+    public String toString(Object number) {
+        return String.valueOf(number);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    // Register listener
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensor,
+                sensorManager.SENSOR_DELAY_UI);
+    }
+
+    // Unregister listener
+    @Override
+    protected void onPause() {
+        sensorManager.unregisterListener(this);
+        super.onPause();
+    }
 
 }
--- app/src/main/java/ch/zli/m335/comtrain/WorkoutActivity.java
+++ app/src/main/java/ch/zli/m335/comtrain/WorkoutActivity.java
@@ -2,11 +2,17 @@ package ch.zli.m335.comtrain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -14,20 +20,38 @@ import org.w3c.dom.Text;
 
 import java.util.Objects;
 
-public class WorkoutActivity extends AppCompatActivity {
+public class WorkoutActivity extends AppCompatActivity{
 
     Intent startMainActivity;
     Intent startPushUpActivity;
     Intent startStepCounterActivity;
 
+    Sensor sensor;
+    SensorManager sensorManager;
+
     //default
     private final int exerciseDefaultGoal = 3;
     private final int roundDefaultGoal = 6;
+    private int exerciseWorkInt = 0;
+    private int roundWorkInt = 0;
+
+    private SensorManager mSensorManager;
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Objects.requireNonNull(mSensorManager).registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        mAccel = 10f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
     }
 
     @Override
@@ -42,6 +66,12 @@ public class WorkoutActivity extends AppCompatActivity {
          * gets all Items from the xml related to Exercise and Round
          */
         createWorkAndGoalViews();
+
+        // activateShakeSensor();
+    }
+
+    public String toString(Object number) {
+        return String.valueOf(number);
     }
 
     public void createNavigation() {
@@ -113,9 +143,42 @@ public class WorkoutActivity extends AppCompatActivity {
         });
     }
 
-    public String toString(Object number) {
-        return String.valueOf(number);
+   /* public void activateShakeSensor() {
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Objects.requireNonNull(mSensorManager).registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        mAccel = 10f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
+    }*/
+
+    private final SensorEventListener mSensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta;
+            if (mAccel > 12) {
+                Toast.makeText(getApplicationContext(), "Shake event detected", Toast.LENGTH_SHORT).show();
+            }
+        }
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+    @Override
+    protected void onResume() {
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        super.onResume();
     }
-
-
+    @Override
+    protected void onPause() {
+        mSensorManager.unregisterListener(mSensorListener);
+        super.onPause();
+    }
 }
