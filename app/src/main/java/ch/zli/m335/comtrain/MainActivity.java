ed5bac9698a32b41ed41e447ac14aa305f945959
Christen, Yanick-MGB-M0365530 <yanick.christen@mgb.ch>
2021-03-04 22:14:55 +0100

    -cleaned up MainActivity.java

--- app/src/main/java/ch/zli/m335/comtrain/MainActivity.java
+++ app/src/main/java/ch/zli/m335/comtrain/MainActivity.java
@@ -9,38 +9,15 @@ import android.view.Window;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-
-    Intent startPushUpActivity;
-    Intent startStepCounterActivity;
-    Intent startWorkoutActivity;
+public class MainActivity extends MyActivty {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        createNavigation();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        startPushUpActivity = new Intent(this, PushUpActivity.class);
-        startStepCounterActivity = new Intent(this, StepCoutnerActivity.class);
-        startWorkoutActivity = new Intent(this, WorkoutActivity.class);
-
-        ImageView PushUpSvg = findViewById(R.id.MainToPushUp);
-        ImageView StepCounterSvg = findViewById(R.id.MainToStepCounter);
-        ImageView WorkoutSvg = findViewById(R.id.MainToWorkout);
-
-        PushUpSvg.setOnClickListener(v -> changeActivities(startPushUpActivity));
-        StepCounterSvg.setOnClickListener(v -> changeActivities(startStepCounterActivity));
-        WorkoutSvg.setOnClickListener(v -> changeActivities(startWorkoutActivity));
-    }
-
-    public void changeActivities(Intent startactivity){
-        startActivity(startactivity);
-        finish();
-    }
 
 }
